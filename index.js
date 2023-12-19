f995768af00d2c97c9d7b8f1b9760753f5e61d2d
jbalmy <108647411+miniisokay@users.noreply.github.com>
2023-12-19 11:52:12 +0530

    Implement animation

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 $('#anim').textillate({ in: { effect: 'fadeInUp' } });
-$('#links a').textillate({ in: { 
-    effect: 'fadeInUp'} });
+$('#links a').textillate({ in: { effect: 'fadeInUp'} });
 
+const tl = new gsap.timeline();
+
+tl.from('.lrow a',{
+    stagger: .2,
+    opacity: 0,
+    x:10
+})
+
+gsap.from('#hero img',{
+    stagger: .2,
+    opacity: 0.2,
+    y: 60,
+},'-=1')
